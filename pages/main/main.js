121150e14b738e4c6b82ae582b3b74f36887961c
thomount <tjpby@163.com>
2019-11-01 21:15:03 +0800

    decorate Main Page
    decorate Main Page
    #29

--- pages/main/main.js
+++ pages/main/main.js
@@ -6,9 +6,9 @@ Page({
    */
   data: {
     imgUrls: [
+      '/image/a3.png',
       '/image/a5.png',
       '/image/a4.png',
-      '/image/a3.png',
       '/image/a2.png',
       '/image/a1.png'
     ],
