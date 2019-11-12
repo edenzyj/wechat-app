a0f00ad2a1d120380fc321ae5810169450fe2ca9
thomount <tjpby@163.com>
2019-11-12 17:34:55 +0800

    add deliver page
    link #41
    add deliver page

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ App({
     userInfo: null,
     regFlag: false,
     loginURL: "http://183.172.140.221:8000/login/",
-    baseURL: "http://183.172.140.221:8000/",
+    baseURL: "http://183.172.141.85:8000/",
 //    baseURL: "https://web-ErrorCode400.app.secoder.net/"
     k: 1.0,
     account: {
