aea92edcfd9cd1e3b2752c3a6beb84b76eac0fa2
张育嘉 <2017011309@secoder.net>
2019-11-13 16:04:20 +0800

    URL
    change baseURL

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ App({
     userInfo: null,
     regFlag: false,
     loginURL: "http://183.172.140.221:8000/login/",
-    baseURL: "http://183.172.141.85:8000/",
-//    baseURL: "https://web-ErrorCode400.app.secoder.net/"
+//    baseURL: "http://183.172.141.85:8000/",
+    baseURL: "https://web-ErrorCode400.app.secoder.net/",
     k: 1.0,
     account: {
       bank: 0,
